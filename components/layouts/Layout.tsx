f644e97e1bb954075a0a416d7d6bde983389bed4
spiddy <dimitris.kapanidis@harbur.io>
2021-05-08 13:46:25 +0200

    Updated index with rss location

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -13,7 +13,7 @@ export default function Layout({ children, title }: LayoutProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Coding Holy Grail - {title}</title>
+        <title>Coding Holy Grail | {title}</title>
 
         <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
         <meta httpEquiv="Content-Language" content="en" />
@@ -35,20 +35,22 @@ export default function Layout({ children, title }: LayoutProps) {
 
         <meta name="HandheldFriendly" content="True" />
         <meta name="MobileOptimized" content="320" />
-        <meta name="description" content="Coding Holy Grail - a personal blog about coding and random thoughts." />
+        <meta name="description" content="Coding Holy Grail | content about containers, cloud native, CI/CD, devops and microservices." />
         <meta name="keywords" content="" />
 
         <meta name="twitter:card" content="photo" />
         <meta name="twitter:url" content="https://codingholygrail.com" />
         <meta name="twitter:title" content="Coding Holy Grail" />
-        <meta name="twitter:description" content="Coding Holy Grail - a personal blog about coding and random thoughts." />
+        <meta name="twitter:description" content="Coding Holy Grail | content about containers, cloud native, CI/CD, devops and microservices." />
         <meta name="twitter:site" content="@dkapanidis" />
         <meta name="twitter:creator" content="@dkapanidis" />
 
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Coding Holy Grail" />
-        <meta property="og:description" content="Coding Holy Grail - a personal blog about coding and random thoughts." />
+        <meta property="og:description" content="Coding Holy Grail | content about containers, cloud native, CI/CD, devops and microservices." />
         <meta property="og:url" content="https://codingholygrail.com" />
+
+        <link rel="alternate" type="application/rss+xml" href="https://codingholygrail.com/feed/index.xml" title="Coding Holy Grail | content about containers, cloud native, CI/CD, devops and microservices."/>
 
         <meta property="fb:admins" content="" />
       </Head>
