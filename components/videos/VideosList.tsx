0b761e81aeb79779f8933445191416672abf4cce
spiddy <dimitris.kapanidis@harbur.io>
2021-05-08 14:42:12 +0200

    added youtube subscribe button

--- components/videos/VideosList.tsx
+++ components/videos/VideosList.tsx
@@ -1,12 +1,18 @@
 import Link from 'next/link'
 import React from 'react'
+import { ImYoutube } from 'react-icons/im'
 import Video from './Video'
 import videos from './videos'
 
 function VideosList() {
   return (
-    <div className="pt-4 w-80">
+    <div className="pt-4 w-80 relative">
       <h1 className="pb-4 text-lg font-semibold">Videos  📹</h1>
+      <a className="absolute right-4 top-5 yt-subscribe-button items-center flex" rel="noopener nofollow" href="https://www.youtube.com/c/dimitriskapanidis?sub_confirmation=1" target="_blank">
+        <span className="flex items-center gap-2 rounded-sm py-1 p-1.5 text-white text-xs" style={{ backgroundColor: 'red' }}>
+          <ImYoutube fill="white" />Subscribe!
+        </span>
+      </a>
       <div className="flex flex-col gap-4">
         {videos.map(video => <VideoRow key={video.content} video={video} />)}
       </div>
