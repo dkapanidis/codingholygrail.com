f407267e97237344ffd9a13a73e5921977bda250
spiddy <dimitris.kapanidis@harbur.io>
2021-05-09 13:14:15 +0200

    added logs to subscribe

--- netlify/functions/subscribe.js
+++ netlify/functions/subscribe.js
@@ -13,6 +13,7 @@ exports.handler = async (event, context) => {
 
   // When the method is POST, the name will no longer be in the event’s
   // queryStringParameters – it’ll be in the event body encoded as a query string
+  console.log(event.body)
   const {email} = event.body;
 
   if (!email) {
