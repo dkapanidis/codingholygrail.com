61a7fd006626d1ac9c3ed63c5992726fa4b44f7a
spiddy <dimitris.kapanidis@harbur.io>
2021-05-09 13:04:45 +0200

    parse body params on netlify function

--- netlify/functions/subscribe.js
+++ netlify/functions/subscribe.js
@@ -5,8 +5,16 @@ mailchimp.setConfig({
   server: process.env.MAILCHIMP_API_SERVER // e.g. us1
 });
 
-exports.handler = async (req, res) => {
-  const { email } = req.body;
+exports.handler = async (event, context) => {
+  // Only allow POST
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+  // When the method is POST, the name will no longer be in the event’s
+  // queryStringParameters – it’ll be in the event body encoded as a query string
+  const params = querystring.parse(event.body);
+  const email = params.email
 
   if (!email) {
     return {
