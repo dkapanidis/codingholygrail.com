039a28f1ea839747efd19f6a4266c7d0331b7b14
spiddy <dimitris.kapanidis@harbur.io>
2021-05-10 14:58:55 +0200

    Added theme inspired by link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className="py-14 space-y-4">
+    <footer className="py-14 space-y-4 relative">
       <div className="space-x-12 text-sm text-gray-500 font-bold text-center">
         <FooterLink href="https://twitter.com/dkapanidis">Twitter</FooterLink>
         <FooterLink href="http://github.com/dkapanidis">GitHub</FooterLink>
@@ -14,6 +14,9 @@ function Footer() {
         <FooterLink href="/rss/feed.xml">RSS Feed</FooterLink>
         <FooterLink href="/rss/atom.xml">Atom Feed</FooterLink>
         <FooterLink href="/rss/feed.json">JSON Feed</FooterLink>
+      </div>
+      <div className="space-x-4 text-xs text-gray-400 text-center absolute right-7 bottom-7">
+        Theme inspired by <a target="_new" className="underline" href="https://midu.dev">midu.dev</a>
       </div>
     </footer>
   )
