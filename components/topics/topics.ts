d90e1207fa81f49f33c5333160d47a2d0f4c07f9
spiddy <dimitris.kapanidis@harbur.io>
2021-05-10 16:09:24 +0200

    Added stateful-apps post

--- components/topics/topics.ts
+++ components/topics/topics.ts
@@ -18,6 +18,12 @@ const topics: { [key: string]: Topic } = {
     text: 'Terminal',
     description: 'Tips & tricks on how to tune the terminal to boost your productivity.',
     icon: '/assets/topics/terminal.svg',
+  },
+  "kubernetes": {
+    id: 'kubernetes',
+    text: 'Kubernetes',
+    description: 'Kubernetes is the de facto platform for running containerized workload on the cloud or your own bare metal.',
+    icon: '/assets/topics/kubernetes.svg',
   }
 }
 
