93b06be52b78a7bc618aa037264d602ac58a18bc
spiddy <dimitris.kapanidis@harbur.io>
2021-05-12 11:40:18 +0200

    Refactor directory structure

--- components/Title.tsx
+++ components/Title.tsx
@@ -26,7 +26,7 @@ function Subtitle({ meta }: SubtitleProps) {
   return (
     <div className="flex items-center text-xs text-gray-400 space-x-4">
       <time dateTime={meta.date}>{postDateTemplate.render(new Date(meta.date))}</time>
-      <Link href={`https://github.com/dkapanidis/codingholygrail.com/edit/main/_posts/${meta.slug}/README.md`}>
+      <Link href={`https://github.com/dkapanidis/codingholygrail.com/edit/main/content/posts/${meta.slug}/README.md`}>
       <a className="flex text-gray-800 items-center gap-1">
         <span>Found an error? Edit the article </span>
         <ImGithub />
