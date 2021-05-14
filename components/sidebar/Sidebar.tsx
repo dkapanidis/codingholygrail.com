c7c59f3bf0a8aa22dc823d789cb427c8244251b2
spiddy <dimitris.kapanidis@harbur.io>
2021-05-14 20:41:16 +0200

    hide toc on mobile

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -3,11 +3,11 @@ import Subscribe from './Subscribe'
 import TableOfContents from './TableOfContents'
 import VideosList from './VideosList'
 
-type Props = {toc?: string[]}
-function Sidebar({toc}:Props) {
+type Props = { toc?: string[] }
+function Sidebar({ toc }: Props) {
   return (
     <div className="flex flex-col flex-shrink lg:w-80 space-y-8">
-      {toc !== undefined && <TableOfContents toc={toc}/>}
+      {toc !== undefined && <TableOfContents toc={toc} />}
       <VideosList />
       <Subscribe />
     </div>
