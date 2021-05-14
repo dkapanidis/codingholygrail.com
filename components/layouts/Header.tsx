bd0f6c505ecf78d995f30f6a6538540129d29eef
spiddy <dimitris.kapanidis@harbur.io>
2021-05-14 17:52:44 +0200

    tune header

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -5,32 +5,30 @@ import Image from 'next/image'
 
 export default function Header() {
   return (
-    <div className="top-0 inset-x-0 z-100 h-30 items-center">
-      <div className="max-w-7xl mx-auto">
-        <div className="flex py-6 items-center">
-          <Link href="/">
-            <a className="flex flex-grow text-lg font-mono font-semibold tracking-widest text-gray-800">
+    <div className="max-w-7xl mx-auto">
+      <div className="flex pt-6 items-center">
+        <Link href="/">
+          <a className="flex items-center text-lg font-mono font-semibold tracking-widest text-gray-800 gap-x-2">
+            <div className="transition transform self-center hover:scale-125">
               <Image
                 src="/images/grail.png"
                 alt="logo"
                 width={30}
-                height={50}
-                className="transition transform hover:scale-125"
+                height={30}
               />
-              <img src="/images/grail.png" width="30" className="mr-6 transition transform hover:scale-125" />
-              <span>Coding Holy Grail</span>
-            </a>
-          </Link>
+            </div>
+            <span className="hover:text-gray-600">Coding Holy Grail</span>
+          </a>
+        </Link>
 
-          <div className="flex-grow" />
-          <div className="flex text-2xl gap-1">
-            <LinkButton to="https://www.youtube.com/channel/UCJxmcXMZnBtxzEcjts2y5dA">
-              <ImYoutube fill="red" />
-            </LinkButton>
-            <LinkButton to="https://twitter.com/dkapanidis">
-              <ImTwitter fill="#1da1f2" />
-            </LinkButton>
-          </div>
+        <div className="flex-grow" />
+        <div className="flex text-2xl gap-1">
+          <LinkButton to="https://www.youtube.com/channel/UCJxmcXMZnBtxzEcjts2y5dA">
+            <ImYoutube fill="red" />
+          </LinkButton>
+          <LinkButton to="https://twitter.com/dkapanidis">
+            <ImTwitter fill="#1da1f2" />
+          </LinkButton>
         </div>
       </div>
     </div>
