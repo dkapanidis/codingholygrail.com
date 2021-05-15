ff04bbd5b6eccbe4b8510dc47b90e37445debae1
spiddy <dimitris.kapanidis@harbur.io>
2021-05-15 15:22:58 +0200

    added social redirects

--- next.config.js
+++ next.config.js
@@ -6,4 +6,28 @@ module.exports = {
   images: {
     domains: ['i2.ytimg.com' ,'i4.ytimg.com'],
   },
+    async redirects() {
+      return [
+        {
+          source: '/youtube',
+          destination: 'https://www.youtube.com/channel/UCJxmcXMZnBtxzEcjts2y5dA',
+          permanent: true,
+        },
+        {
+          source: '/twitter',
+          destination: 'https://twitter.com/dkapanidis',
+          permanent: true,
+        },
+        {
+          source: '/linkedin',
+          destination: 'https://www.linkedin.com/in/kapanidis/',
+          permanent: true,
+        },
+        {
+          source: '/github',
+          destination: 'https://github.com/dkapanidis',
+          permanent: true,
+        }
+      ]
+    },
 }
